fd994ef8b107b6848c6a293fe7c886b500ef72d5
Kevin Anderson <kraatw2008@yahoo.com>
2018-09-20 12:04:43 -0500

    fixing up requesting android premissions

--- CrossCam.Droid/MainActivity.cs
+++ CrossCam.Droid/MainActivity.cs
@@ -1,10 +1,12 @@
-﻿using Android;
+﻿using System.Linq;
+using Android;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Support.V4.App;
 using Android.Support.V4.Content;
 using Android.Views;
+using Java.Lang;
 using Xamarin.Forms;
 
 namespace CrossCam.Droid
@@ -16,11 +18,14 @@ namespace CrossCam.Droid
         MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
         ScreenOrientation = ScreenOrientation.Sensor)]
-    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ActivityCompat.IOnRequestPermissionsResultCallback
     {
         public OrientationHelper OrientationHelper;
 
         internal static MainActivity Instance { get; private set; }
+
+        private const int CAMERA_PERMISSION_REQUEST_CODE = 50;
+        private const int WRITE_TO_STORAGE_REQUEST_CODE = 51;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -38,9 +43,34 @@ namespace CrossCam.Droid
 
             if (ContextCompat.CheckSelfPermission(Forms.Context, Manifest.Permission.Camera) != (int)Permission.Granted)
             {
-                ActivityCompat.RequestPermissions(Instance, new[] { Manifest.Permission.Camera }, 50);
+                ActivityCompat.RequestPermissions(Instance, new[] { Manifest.Permission.Camera }, CAMERA_PERMISSION_REQUEST_CODE);
             }
-            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && 
+                grantResults.Contains(Permission.Granted))
+            {
+                if (ContextCompat.CheckSelfPermission(Forms.Context, Manifest.Permission.WriteExternalStorage) != (int)Permission.Granted)
+                {
+                    ActivityCompat.RequestPermissions(Instance, new[] { Manifest.Permission.WriteExternalStorage }, WRITE_TO_STORAGE_REQUEST_CODE);
+                }
+                else
+                {
+                    LoadApplication(new App());
+                }
+            }
+            else if (requestCode == WRITE_TO_STORAGE_REQUEST_CODE && 
+                     grantResults.Contains(Permission.Granted))
+            {
+                LoadApplication(new App());
+            }
+            else
+            {
+                JavaSystem.Exit(0);
+            }
         }
     }
 }
