97aa3455eb92e404762e54505ee197049f9b80bd
Kevin Anderson <kraatw2008@yahoo.com>
2018-11-06 23:52:11 -0600

    refactored a little bit and added a fixed width border which appears when both sides are captured

--- CrossCam/Page/CameraPage.xaml.cs
+++ CrossCam/Page/CameraPage.xaml.cs
@@ -77,105 +77,16 @@ namespace CrossCam.Page
 
 	        if (_viewModel.LeftByteArray != null)
 	        {
-	            DrawImageOnCanvas(e, _viewModel.LeftByteArray, true);
+	            var withBorder = _viewModel.RightByteArray != null;
+	            DrawTool.DrawImageOnCanvas(e.Info, e.Surface.Canvas, _viewModel.LeftByteArray, true, withBorder);
 	        }
 
 	        if (_viewModel.RightByteArray != null)
 	        {
-	            DrawImageOnCanvas(e, _viewModel.RightByteArray, false);
+	            var withBorder = _viewModel.LeftByteArray != null;
+	            DrawTool.DrawImageOnCanvas(e.Info, e.Surface.Canvas, _viewModel.RightByteArray, false, withBorder);
 	        }
         }
-
-	    private static void DrawImageOnCanvas(SKPaintSurfaceEventArgs e, byte[] byteArray, bool isLeft)
-	    {
-	        var bitmap = GetBitmapAndCorrectOrientation(byteArray);
-	        var imageAspectRatio = bitmap.Height / (1f * bitmap.Width);
-	        var screenWidth = e.Info.Width;
-	        var screenHeight = e.Info.Height;
-	        float previewHeight;
-	        float previewWidth;
-	        float previewX;
-	        float previewY;
-	        if (screenHeight > screenWidth) // screen portrait
-	        {
-	            previewWidth = screenWidth / 2f;
-	            previewHeight = imageAspectRatio * previewWidth;
-	            previewX = 0;
-	            previewY = (screenHeight - previewHeight) / 2f;
-	        }
-	        else // screen landscape
-	        {
-	            if (bitmap.Height > bitmap.Width) // image portrait
-	            {
-	                previewHeight = screenHeight;
-	                previewWidth = previewHeight / imageAspectRatio;
-	                previewX = screenWidth / 2f - previewWidth;
-	                previewY = 0;
-	            }
-	            else // image landscape
-	            {
-	                previewWidth = screenWidth / 2f;
-	                previewHeight = previewWidth * imageAspectRatio;
-	                previewX = 0;
-	                previewY = (screenHeight - previewHeight) / 2f;
-	            }
-	        }
-
-	        e.Surface.Canvas.DrawBitmap(bitmap,
-	            SKRect.Create(0, 0, bitmap.Width, bitmap.Height),
-	            SKRect.Create(isLeft ? previewX : screenWidth / 2f, previewY, previewWidth, previewHeight));
-            bitmap.Dispose();
-        }
-
-	    private static SKBitmap GetBitmapAndCorrectOrientation(byte[] byteArray)
-	    {
-	        SKCodecOrigin origin;
-
-	        using (var stream = new MemoryStream(byteArray))
-	        using (var data = SKData.Create(stream))
-	        using (var codec = SKCodec.Create(data))
-	        {
-	            origin = codec.Origin;
-	        }
-
-	        switch (origin)
-	        {
-	            case SKCodecOrigin.BottomRight:
-	                return BitmapRotate180(SKBitmap.Decode(byteArray));
-	            case SKCodecOrigin.RightTop:
-	                return BitmapRotate90(SKBitmap.Decode(byteArray));
-	            default:
-	                return SKBitmap.Decode(byteArray);
-	        }
-        }
-
-	    private static SKBitmap BitmapRotate90(SKBitmap originalBitmap)
-	    {
-	        var rotated = new SKBitmap(originalBitmap.Height, originalBitmap.Width);
-
-	        using (var surface = new SKCanvas(rotated))
-	        {
-	            surface.Translate(rotated.Width, 0);
-	            surface.RotateDegrees(90);
-	            surface.DrawBitmap(originalBitmap, 0, 0);
-	        }
-
-	        return rotated;
-	    }
-
-	    private static SKBitmap BitmapRotate180(SKBitmap originalBitmap)
-	    {
-	        var rotated = new SKBitmap(originalBitmap.Width, originalBitmap.Height);
-
-	        using (var surface = new SKCanvas(rotated))
-	        {
-	            surface.Translate(rotated.Width, rotated.Height);
-	            surface.RotateDegrees(180);
-	            surface.DrawBitmap(originalBitmap, 0, 0);
-	        }
-
-	        return rotated;
-	    }
 
         private void ResetGuides()
         {
---
+++ CrossCam/ViewModel/DrawTool.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using SkiaSharp;
+
+namespace CrossCam.ViewModel
+{
+    public class DrawTool
+    {
+        private const int BORDER_WIDTH = 10;
+
+        public static void DrawImageOnCanvas(SKImageInfo info, SKCanvas canvas, byte[] byteArray, bool isLeft, bool withBorder)
+        {
+            var bitmap = GetBitmapAndCorrectOrientation(byteArray);
+            var imageAspectRatio = bitmap.Height / (1f * bitmap.Width);
+            var screenWidth = info.Width;
+            var screenHeight = info.Height;
+            float previewHeight;
+            float previewWidth;
+            float previewX;
+            float previewY;
+            if (screenHeight > screenWidth) // screen portrait
+            {
+                previewWidth = screenWidth / 2f;
+                previewHeight = imageAspectRatio * previewWidth;
+                previewX = 0;
+                previewY = (screenHeight - previewHeight) / 2f;
+            }
+            else // screen landscape
+            {
+                if (bitmap.Height > bitmap.Width) // image portrait
+                {
+                    previewHeight = screenHeight;
+                    previewWidth = previewHeight / imageAspectRatio;
+                    previewX = screenWidth / 2f - previewWidth;
+                    previewY = 0;
+                }
+                else // image landscape
+                {
+                    previewWidth = screenWidth / 2f;
+                    previewHeight = previewWidth * imageAspectRatio;
+                    previewX = 0;
+                    previewY = (screenHeight - previewHeight) / 2f;
+                }
+            }
+
+            canvas.DrawBitmap(bitmap,
+                SKRect.Create(0, 0, bitmap.Width, bitmap.Height),
+                SKRect.Create(
+                    (isLeft ? previewX : screenWidth / 2f) + (withBorder ? BORDER_WIDTH : 0),
+                    previewY + (withBorder ? BORDER_WIDTH : 0),
+                    previewWidth - (withBorder ? BORDER_WIDTH * 2 : 0),
+                    previewHeight - (withBorder ? BORDER_WIDTH * 2 : 0)));
+            bitmap.Dispose();
+        }
+
+        private static SKBitmap GetBitmapAndCorrectOrientation(byte[] byteArray)
+        {
+            SKCodecOrigin origin;
+
+            using (var stream = new MemoryStream(byteArray))
+            using (var data = SKData.Create(stream))
+            using (var codec = SKCodec.Create(data))
+            {
+                origin = codec.Origin;
+            }
+
+            switch (origin)
+            {
+                case SKCodecOrigin.BottomRight:
+                    return BitmapRotate180(SKBitmap.Decode(byteArray));
+                case SKCodecOrigin.RightTop:
+                    return BitmapRotate90(SKBitmap.Decode(byteArray));
+                default:
+                    return SKBitmap.Decode(byteArray);
+            }
+        }
+
+        private static SKBitmap BitmapRotate90(SKBitmap originalBitmap)
+        {
+            var rotated = new SKBitmap(originalBitmap.Height, originalBitmap.Width);
+
+            using (var surface = new SKCanvas(rotated))
+            {
+                surface.Translate(rotated.Width, 0);
+                surface.RotateDegrees(90);
+                surface.DrawBitmap(originalBitmap, 0, 0);
+            }
+
+            return rotated;
+        }
+
+        private static SKBitmap BitmapRotate180(SKBitmap originalBitmap)
+        {
+            var rotated = new SKBitmap(originalBitmap.Width, originalBitmap.Height);
+
+            using (var surface = new SKCanvas(rotated))
+            {
+                surface.Translate(rotated.Width, rotated.Height);
+                surface.RotateDegrees(180);
+                surface.DrawBitmap(originalBitmap, 0, 0);
+            }
+
+            return rotated;
+        }
+    }
+}
