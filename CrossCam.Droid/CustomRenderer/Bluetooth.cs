d36f89132c166a68f5f6686a1054c15c45cc972c
Kevin Anderson <kraatw2008@yahoo.com>
2019-09-10 21:10:56 -0500

    bluetooth experimental base

---
+++ CrossCam.Droid/CustomRenderer/Bluetooth.cs
@@ -0,0 +1,14 @@
+﻿using CrossCam.Droid.CustomRenderer;
+using CrossCam.Wrappers;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Bluetooth))]
+namespace CrossCam.Droid.CustomRenderer
+{
+    public class Bluetooth : IBluetooth
+    {
+        public async void What()
+        {
+        }
+    }
+}
