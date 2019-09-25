d5cd3182fc52298801f2d9f0a30d208e7505a071
Kevin Anderson <kraatw2008@yahoo.com>
2019-09-24 23:42:04 -0500

    adding in bunch of 'email me' links

---
+++ CrossCam/CustomElement/EmailMeLabel.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CrossCam.CustomElement
+{
+    public partial class EmailMeLabel
+    {
+        public EmailMeLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("mailto:me@kra2008.com?subject=CrossCam+feedback"));
+        }
+    }
+}
