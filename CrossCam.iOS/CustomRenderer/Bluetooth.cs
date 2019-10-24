2aec9d72d21887e72856e88bf513228bfbbdaa33
Kevin Anderson <kraatw2008@yahoo.com>
2019-10-23 23:26:36 -0500

    starting some iOS stuff, no idea what i'm doing, seems like debugger isn't connecting

--- CrossCam.iOS/CustomRenderer/Bluetooth.cs
+++ CrossCam.iOS/CustomRenderer/Bluetooth.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
+using CoreBluetooth;
+using CoreFoundation;
 using CrossCam.iOS.CustomRenderer;
 using CrossCam.Wrappers;
+using Foundation;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(Bluetooth))]
@@ -10,70 +17,107 @@ namespace CrossCam.iOS.CustomRenderer
 {
     public class Bluetooth : IBluetooth
     {
+        public static readonly ObservableCollection<CBPeripheral> AvailableDevices =
+            new ObservableCollection<CBPeripheral>();
+
+        private CBCentralManager _centralManager;
+
+        public event EventHandler<PartnerDevice> DeviceDiscovered;
+
+        public Bluetooth()
+        {
+            AvailableDevices.CollectionChanged += (sender, args) =>
+            {
+                if (args.Action == NotifyCollectionChangedAction.Add)
+                {
+                    foreach (var newItem in args.NewItems)
+                    {
+                        var newDevice = (CBPeripheral)newItem;
+                        OnDeviceDiscovered(new PartnerDevice
+                        {
+                            Name = newDevice.Name ?? "Unnamed",
+                            Address = "idk" //TODO
+                        });
+                    }
+                }
+            };
+        }
+
+        private void OnDeviceDiscovered(PartnerDevice e)
+        {
+            var handler = DeviceDiscovered;
+            handler?.Invoke(this, e);
+        }
+
         public bool IsConnected()
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         public void Disconnect()
         {
-            throw new NotImplementedException();
         }
 
         public Task<bool> RequestBluetoothPermissions()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public Task<bool> RequestLocationPermissions()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public bool IsBluetoothSupported()
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public Task<bool> TurnOnBluetooth()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public Task<bool> TurnOnLocationServices()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public List<PartnerDevice> GetPairedDevices()
         {
-            throw new NotImplementedException();
+            return Enumerable.Empty<PartnerDevice>().ToList();
         }
 
         public bool BeginSearchForDiscoverableDevices()
         {
-            throw new NotImplementedException();
+            var managerDelegate = new BluetoothManagerDelegate();
+            _centralManager = new CBCentralManager(managerDelegate, DispatchQueue.DefaultGlobalQueue,
+                new CBCentralInitOptions());
+            _centralManager.ScanForPeripherals(CBUUID.FromString(PartnerDevice.SDP_UUID));
+            return false;
         }
 
-        public event EventHandler<PartnerDevice> DeviceDiscovered;
         public Task<bool> BecomeDiscoverable()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public Task<bool?> ListenForConnections()
         {
-            throw new NotImplementedException();
+            var taskCompletionSource = new TaskCompletionSource<bool?>();
+            return taskCompletionSource.Task;
         }
 
         public Task<bool> AttemptConnection(PartnerDevice partnerDevice)
         {
-            throw new NotImplementedException();
+            var matchingDevice = AvailableDevices.First(d => d.Name == partnerDevice.Name);
+            _centralManager.ConnectPeripheral(matchingDevice);
+            _centralManager.StopScan();
+            return Task.FromResult(true);
         }
 
         public void ForgetDevice(PartnerDevice partnerDevice)
         {
-            throw new NotImplementedException();
         }
 
         public Task<bool> SendPreviewFrame(byte[] preview)
@@ -84,6 +128,29 @@ namespace CrossCam.iOS.CustomRenderer
         public Task<byte[]> Capture(int countdownSeconds)
         {
             throw new NotImplementedException();
+        }
+
+        private class BluetoothManagerDelegate : CBCentralManagerDelegate
+        {
+            public override void UpdatedState(CBCentralManager central)
+            {
+                Debug.WriteLine("Updated state: " + central.State);
+            }
+
+            public override void DiscoveredPeripheral(CBCentralManager central, CBPeripheral peripheral, NSDictionary advertisementData,
+                NSNumber RSSI)
+            {
+                base.DiscoveredPeripheral(central, peripheral, advertisementData, RSSI);
+                Debug.WriteLine("Discovered device: " + peripheral.Name);
+                AvailableDevices.Add(peripheral);
+                var peripheralDelegate = new BluetoothPeripheralDelgate();
+                peripheral.Delegate = peripheralDelegate;
+            }
+        }
+
+        private class BluetoothPeripheralDelgate : CBPeripheralDelegate
+        {
+
         }
     }
 }
